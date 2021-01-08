332cb057e5533ea8863c86052a10efd9345e4a50
Diego Leonardo Urban <diego.urban88@gmail.com>
2021-01-07 22:05:21 -0300

    teste

--- CMP.16.0.2 - Aprendizado de Máquina II - Aprendizado Não Supervisionado/trabalho-final/pca/person.py
+++ CMP.16.0.2 - Aprendizado de Máquina II - Aprendizado Não Supervisionado/trabalho-final/pca/person.py
@@ -2,4 +2,8 @@
 
 # https://github.com/lobokoch/unsupervised-learning2/blob/main/pca-eigenfaces/src/br/furb/Person.java
 class Person():
-    pass
+
+    def __init__(self, id, label, data):
+        self.id = id
+        self.label = label
+        self.data = data
